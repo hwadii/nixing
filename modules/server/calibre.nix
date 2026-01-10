e0a8f24e92fe8729b2921b26db34998432d46cfe
Wadii Hajji <hajji.wadii@yahoo.fr>
2026-01-10 18:37:22 +0100

    calibre: add kobo depenedencies

--- modules/server/calibre.nix
+++ modules/server/calibre.nix
@@ -1,11 +1,21 @@
+{ pkgs, ... }:
+
 {
+  environment.systemPackages = [ pkgs.calibre ];
+
   services.calibre-web = {
     enable = true;
     user = "wadii";
     options = {
       enableBookUploading = true;
+      enableKepubify = true;
+      enableBookConversion = true;
       calibreLibrary = "/mnt/a/books";
     };
+    package =
+      pkgs.calibre-web.overridePythonAttrs (old: {
+        dependencies = old.dependencies ++ old.optional-dependencies.kobo;
+      });
     openFirewall = true;
     listen = {
       ip = "0.0.0.0";
