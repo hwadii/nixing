90a99a25a35b15d16592959dd87363ee74d94ffe
Wadii Hajji <hajji.wadii@yahoo.fr>
2026-04-22 19:51:05 +0200

    modules/server: fix dns

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -1,12 +1,35 @@
+{ config, pkgs, ... }:
+
 {
   services.caddy = {
     enable = true;
-    virtualHosts = {
-      "watch.exondation.com" = {
-        extraConfig = ''
-          reverse_proxy localhost:8096
-        '';
-      };
+    package = pkgs.caddy.withPlugins {
+      plugins = [ "github.com/caddy-dns/porkbun@v0.3.1" ];
+      hash = "sha256-GKtPd73R/7ggu3fBFilkhyKi45lkAAQFsftwg5pmWW4=";
     };
+    virtualHosts."*.h.exondation.com".extraConfig = ''
+      tls {
+        dns porkbun {
+          api_key {env.PORKBUN_API_KEY}
+          api_secret_key {env.PORKBUN_API_SECRET_KEY}
+        }
+      }
+      @watch host watch.h.exondation.com
+      handle @watch {
+        reverse_proxy 127.0.0.1:8096
+      }
+
+      @kobo host kobo.h.exondation.com
+      handle @kobo {
+        reverse_proxy 127.0.0.1:8083
+      }
+
+      @sync host sync.h.exondation.com
+      handle @sync {
+        reverse_proxy 127.0.0.1:8384
+      }
+    '';
   };
+
+  systemd.services.caddy.serviceConfig.EnvironmentFile = "/var/lib/secrets/porkbun.env";
 }
--- modules/server/dns.nix
+++ modules/server/dns.nix
@@ -1,80 +1,33 @@
 { pkgs, ... }:
 
 let
-  lanIp = "192.168.1.150";
   tailscaleIp = "100.68.65.84";
 in
 {
-  networking.nameservers = [ "127.0.0.1" ];
+  networking.nameservers = [ "1.1.1.1" "9.9.9.9" ];
 
   networking.firewall = {
     interfaces.eno1.allowedUDPPorts = [ 53 ];
-    interfaces.eno1.allowedTCPPorts = [ 53 ];
+    interfaces.eno1.allowedTCPPorts = [ 53 80 443 ];
 
     interfaces.tailscale0.allowedUDPPorts = [ 53 ];
     interfaces.tailscale0.allowedTCPPorts = [ 53 ];
   };
 
   services.resolved = {
-    enable = true;
-    dnssec = "false";
-    extraConfig = ''
-      DNSStubListener=no
-    '';
+    enable = false;
   };
 
   services.dnsmasq = {
-    enable = false;
+    enable = true;
     settings = {
-      listen-address = [ "127.0.0.1" lanIp tailscaleIp ];
+      server = [ "1.1.1.1" "9.9.9.9" ];
+      interface = [ "tailscale0" "lo" ];
       bind-interfaces = true;
-      domain = "exondation.com";
-      address = [
-        "/exondation.com/${lanIp}"
-      ];
-      dhcp-range = "192.168.1.50,192.168.1.200,12h";
-      dhcp-authoritative = true;
-      expand-hosts = true;
-      cache-size = 1000;
-      no-resolv = false;
-    };
-  };
-
-  environment.etc."dnsmasq-lan.conf".text = ''
-    interface=eno1
-    bind-interfaces
-
-    domain=exondation.com
-
-    address=/exondation.com/${tailscaleIp}
-  '';
-
-  environment.etc."dnsmasq-ts.conf".text = ''
-    interface=tailscale0
-    bind-interfaces
-
-    domain=exondation.com
-
-    address=/exondation.com/${tailscaleIp}
-  '';
-
-  systemd.services.dnsmasq-lan = {
-    description = "dnsmasq (LAN)";
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      ExecStart = "${pkgs.dnsmasq}/bin/dnsmasq --keep-in-foreground --conf-file=/etc/dnsmasq-lan.conf";
-      Restart = "always";
-    };
-  };
-
-  systemd.services.dnsmasq-ts = {
-    description = "dnsmasq (Tailscale)";
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      ExecStart = "${pkgs.dnsmasq}/bin/dnsmasq --keep-in-foreground --conf-file=/etc/dnsmasq-ts.conf";
-      Restart = "always";
+      address = [ "/h.exondation.com/${tailscaleIp}" ];
+      domain-needed = true;
+      bogus-priv = true;
+      no-resolv = true;
     };
   };
 }
