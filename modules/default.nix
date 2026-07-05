65bb2707d815876690f2e5310caa2c939da584c4
Wadii Hajji <wadii.hajji@proton.me>
2026-07-05 16:45:48 +0200

    xdg: set GOPATH in XDG_DATA_HOME

--- modules/default.nix
+++ modules/default.nix
@@ -21,6 +21,8 @@
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_DATA_HOME = "$HOME/.local/share";
     XDG_STATE_HOME = "$HOME/.local/state";
+
+    GOPATH = "$HOME/.local/share/go";
   };
 
   xdg.userDirs = {
