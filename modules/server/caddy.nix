3492e9243b4732449150ca138c5a721bcfdd17e0
Wadii Hajji <wadii.hajji@proton.me>
2026-08-30 11:05:11 +0200

    server: fix caddy hash

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   services.caddy = {
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/porkbun@v0.3.1" ];
-      hash = "sha256-MlKX2obWac+jP4j9UHFMxsY/DRaqw9JCVAdI7erhFwo=";
+      hash = "sha256-YmKKk5sSOVtv3fwF3kLZtxGL8YpQmHLR59eOcFnhfUo=";
     };
     virtualHosts."*.h.exondation.com".extraConfig = ''
       tls {
